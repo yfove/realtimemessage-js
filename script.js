2a16ec0a216be7fc0e98bb5d65ee11aac6cab7ad
curtis chat <curtis.ch.chat@gmail.com>
2019-12-11 17:18:02 -0700

    resolved firebase database error

--- script.js
+++ script.js
@@ -19,3 +19,53 @@ chatApp.config = () => {
     //getting reference to chat object in database
     chatApp.chat = chatApp.database.ref(`/chat`);
 };
+
+chatApp.init = () => {
+    //Add message
+    //on form submission
+    $('.chat').on('submit', function (e) {
+        e.preventDefault(); //when submit prevent refresh screen
+        //save input from form
+        const message = {
+            name: chatApp.nameField.val(),
+            text: chatApp.messageField.val(),
+        };
+        console.log(message);
+        //pushing to database
+        chatApp.chat.push(message);
+        chatApp.messageField.val("");
+    });
+
+    //listen to changes in firebase database, listenining to the chat reference to see if anything is added
+    //limited to 10 messages
+    //limitToLast is only for firebase
+    //child_added is only for firebase
+    chatApp.chat.limitToLast(10).on('child_added', function (message) {
+        const id = message.key;
+        const data = message.val();
+        const text = data.text;
+        //name is optional
+        const name = data.name || 'Unknown';
+        const li = `<li>
+                        <strong>${name}</strong>
+                        <p>${text}</p>
+                        <button class="delete" id=${id}>
+                            <i class="fas fa-times-circle"></i>
+                        </button>
+                    </li>`;
+
+        //adds new message to page
+        chatApp.messageList.append(li);
+        //always scroll to the bottom;
+        chatApp.messageList[0].scrollTop = chatApp.messageList[0].scrollHeight;
+    });
+};
+
+$(function () {
+    chatApp.config();
+    chatApp.messageField = $('#messageInput');
+    chatApp.nameField = $('#nameInput');
+    chatApp.messageList = $('.messages');
+    //init will have all the event handlers
+    chatApp.init();
+});
